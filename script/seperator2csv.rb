02121f34a64e24660b65b742bb91760ddf6bea00
Lichao Chen <chenlichao@gmail.com>
2013-03-19 23:49:25 -0700

    seperator2csv done, going to get groundtruth;)

--- script/seperator2csv.rb
+++ script/seperator2csv.rb
@@ -8,8 +8,12 @@ sep<<clu.count
 
 
 clusize = (1..(sep.size-1)).map{|i|sep[i]-sep[i-1]}
-cluset = Set.new
-clusize.each{|x| cluset<<x}
-puts clusize.inspect
-puts cluset.inspect
+rt=clusize.each_with_index.flat_map{|x,i| [i]*x}
+vecs = Array.new(sep.count-1){Array.new(clu.max+1,0)}
+(clu.zip rt).each{|cid, iid| vecs[iid][cid]+=1}
 
+File.open(csvfn,"w") do |fout|
+	vecs.each do |img|
+		fout.puts img.join(",")
+	end
+end
