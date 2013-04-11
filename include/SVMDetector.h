649fbf0dd15588e016eaf33f0d38374181ca6470
Lichao Chen <chenlichao@gmail.com>
2013-04-10 19:12:51 -0700

    svm done

--- include/SVMDetector.h
+++ include/SVMDetector.h
@@ -7,9 +7,11 @@ PatchDetector
 {
 public:
 	SVMDetector(string fname);
+	SVMDetector(const Mat& feavec, const vector<int>& label);
     ~SVMDetector(void);
 	virtual void detect(const vector<float>& vec, int&c, float&score) override;
 private:
+	CvSVM SVM;
 	bool load(string fname);
 	float b;//constant term in svm
 	float svm[vlen];//svm coeff;
--- src/SVMDetector.cpp
+++ src/SVMDetector.cpp
@@ -8,42 +8,38 @@ using namespace std;
 
 
 SVMDetector::SVMDetector(string fname){
-//    ifstream fin(fname);
-	ifstream fin(fname.c_str());
+	ifstream fin(fname);
 	string line;
 	getline(fin,line);
-    
-//	b = stof(line);
-    b = atof(line.c_str());
-    
+
+	b = stof(line);
+
 	getline(fin,line);
 	stringstream ss(line);
 	string item;
 	for(int i=0;i<vlen;i++){
 		getline(ss, item, ',');
-//		svm[i]=stof(item);
-        svm[i]=atof(item.c_str());
+		svm[i]=stof(item);
 	}
 	fin.close();
 }
 
+SVMDetector::SVMDetector(const Mat& feavec, const vector<int>& label){
+	CvSVMParams params;
+	params.svm_type    = CvSVM::C_SVC;
+	params.kernel_type = CvSVM::LINEAR;
+	params.term_crit   = cvTermCriteria(CV_TERMCRIT_ITER, 100, 1e-6);
+	
+	Mat labelsMat(label);
+	// Train the SVM
+	SVM.train(feavec, labelsMat, Mat(), Mat(), params);
 
+}
 SVMDetector::~SVMDetector(void)
 {
 }
 
 void SVMDetector::detect(const vector<float>& vec, int& cat, float&score){
-	cout<<"using HumanDetector"<<endl;
-	float sum=-b;
-	for(int i = 0;i< vlen;i++){
-		sum+=svm[i]*vec[i];
-	}
-	cout<<sum<<endl;
-	if (sum>0){
-		cat = 1;
-	}
-	else{
-		cat= -1;
-	}
-	score = sum;
+	score = SVM.predict(Mat(vec).t(),true);
+	cat = score>0?1:-1;
 }
