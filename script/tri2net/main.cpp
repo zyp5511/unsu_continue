7cac7964c5a836d6e39c3f7c82c2600552595ba0
Lichao Chen <lichao@fenghua>
2013-08-09 19:15:25 -0700

    tri2net finished

--- script/tri2net/main.cpp
+++ script/tri2net/main.cpp
@@ -1,5 +1,8 @@
 #include<fstream>
+#include<sstream>
+#include<iostream>
 using namespace std;
+
 int main(int argc, char** argv){
 	if(argc<3){
 		cerr<<"input and output filename required"<<endl;
@@ -9,10 +12,23 @@ int main(int argc, char** argv){
 		ofstream fout(argv[2]);
 		string line;
 
-		iterate by line
+		//iterate by line
+		int i = 0;
 		while(getline(fin, line)){
+			i++;
+			stringstream ss(line);
+			int j=i;
+			float dis;
+			ss>>dis;
+			while(ss>>dis){
+				j++;
+				if (dis<47){
+					fout<<i<<"\t"<<j<<"\t"<<dis<<endl;
+				}
+			}
 		}	
-
+		fin.close();
+		fout.close();
 	}
 	return 0;
 }
